e549baf3baa8f1f91c131a76723fea4dfd4a2d35
martin2844 <martin2844@gmail.com>
2024-05-29 17:15:23 +0200

    Fix OpenAi model and usage

--- src/services/openai.ts
+++ src/services/openai.ts
@@ -6,21 +6,39 @@ import { BookDetails } from "@ctypes/books";
 const openai = new OpenAI({ apiKey: OPENAI_KEY });
 
 const getAIbookDescription = async (book: BookDetails) => {
-  const aiResponse = await openai.completions.create({
-    model: "gpt-3.5-turbo-instruct",
-    prompt: `Give me a 50 words description about the book ${book.title} by ${book.author} in spanish, do not translate the title nor the author if you include it in the response`,
+  const aiResponse = await openai.chat.completions.create({
+    model: "gpt-4o",
+    messages: [
+      {
+        role: "system",
+        content: "You are a helpful assistant.",
+      },
+      {
+        role: "user",
+        content: `Give me a 50-80 words description about the book ${book.title} by ${book.author} in Spanish. Do not translate the title nor the author if you include it in the response.`,
+      },
+    ],
     max_tokens: 512,
   });
-  return aiResponse.choices[0].text;
+  return aiResponse.choices[0].message.content;
 };
 
 const getAIKeywords = async (book: BookDetails) => {
-  const aiResponse = await openai.completions.create({
-    model: "gpt-3.5-turbo-instruct",
-    prompt: `Give me 5 keywords about the book ${book.title} by ${book.author}, keywords examples are 'java', 'php', 'machine-learning', 'ai', 'architecture', 'competitive programming', 'web dev', keywords should be of a general nature, dont give me very niche results, reply in a javascript array format like this ['keyword1', 'keyword2', 'keyword3']`,
+  const aiResponse = await openai.chat.completions.create({
+    model: "gpt-4o",
+    messages: [
+      {
+        role: "system",
+        content: "You are a helpful assistant.",
+      },
+      {
+        role: "user",
+        content: `Give me 5 keywords about the book ${book.title} by ${book.author}. Keywords examples are 'java', 'php', 'machine-learning', 'ai', 'architecture', 'competitive programming', 'web dev'. They are comparable to hashtags. Keywords should be of a general nature, don't give me very niche results. Reply in an array format like this ['keyword1', 'keyword2', 'keyword3'] Its imperative you always mantain this format.`,
+      },
+    ],
     max_tokens: 512,
   });
-  const keywordsString = aiResponse.choices[0].text;
+  const keywordsString = aiResponse.choices[0].message.content;
   // Remove the square brackets and single quotes from the string
   const cleanString = keywordsString.replace(/[\[\]']+/g, "");
   // Split the string into an array
@@ -31,12 +49,21 @@ const getAIKeywords = async (book: BookDetails) => {
 };
 
 const getAiSubject = async (book: BookDetails) => {
-  const aiResponse = await openai.completions.create({
-    model: "gpt-3.5-turbo-instruct",
-    prompt: `Give me the main subject about the book ${book.title} by ${book.author}, should be the best word that describes what the book is about`,
+  const aiResponse = await openai.chat.completions.create({
+    model: "gpt-4o",
+    messages: [
+      {
+        role: "system",
+        content: "You are a helpful assistant.",
+      },
+      {
+        role: "user",
+        content: `Give me the main subject about the book ${book.title} by ${book.author}. It should be the best word that describes what the book is about. Only one word is expected`,
+      },
+    ],
     max_tokens: 512,
   });
-  return aiResponse.choices[0].text;
+  return aiResponse.choices[0].message.content;
 };
 
 export { getAIbookDescription, getAIKeywords, getAiSubject };
